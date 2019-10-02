e7c3a5651a374b6d40d5ed8cb69605600708b16c
komok113 <abdulhakim.dev@gmail.com>
2019-10-02 20:42:38 +0300

    release w1

---
+++ Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Google_Maps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txt_ipInfo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_city = new System.Windows.Forms.Label();
+            this.lbl_region = new System.Windows.Forms.Label();
+            this.lbl_country = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_longitude = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbl_latitude = new System.Windows.Forms.Label();
+            this.lbl_timezona = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txt_ipInfo);
+            this.splitContainer1.Panel1.Controls.Add(this.label8);
+            this.splitContainer1.Panel1.Controls.Add(this.label7);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_timezona);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_latitude);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_longitude);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_city);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_region);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_country);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Search);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(755, 356);
+            this.splitContainer1.SplitterDistance = 265;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txt_ipInfo
+            // 
+            this.txt_ipInfo.Location = new System.Drawing.Point(15, 57);
+            this.txt_ipInfo.Name = "txt_ipInfo";
+            this.txt_ipInfo.Size = new System.Drawing.Size(138, 23);
+            this.txt_ipInfo.TabIndex = 4;
+            this.txt_ipInfo.TextChanged += new System.EventHandler(this.Txt_ipInfo_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "City";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Region";
+            // 
+            // lbl_city
+            // 
+            this.lbl_city.AutoSize = true;
+            this.lbl_city.Location = new System.Drawing.Point(94, 170);
+            this.lbl_city.Name = "lbl_city";
+            this.lbl_city.Size = new System.Drawing.Size(12, 17);
+            this.lbl_city.TabIndex = 3;
+            this.lbl_city.Text = "-";
+            // 
+            // lbl_region
+            // 
+            this.lbl_region.AutoSize = true;
+            this.lbl_region.Location = new System.Drawing.Point(94, 139);
+            this.lbl_region.Name = "lbl_region";
+            this.lbl_region.Size = new System.Drawing.Size(12, 17);
+            this.lbl_region.TabIndex = 3;
+            this.lbl_region.Text = "-";
+            // 
+            // lbl_country
+            // 
+            this.lbl_country.AutoSize = true;
+            this.lbl_country.Location = new System.Drawing.Point(94, 111);
+            this.lbl_country.Name = "lbl_country";
+            this.lbl_country.Size = new System.Drawing.Size(12, 17);
+            this.lbl_country.TabIndex = 3;
+            this.lbl_country.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Country";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP Info";
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_Search.FlatAppearance.BorderSize = 0;
+            this.btn_Search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Search.Location = new System.Drawing.Point(159, 57);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(91, 23);
+            this.btn_Search.TabIndex = 2;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = false;
+            this.btn_Search.Click += new System.EventHandler(this.Btn_Search_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(486, 356);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 26);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.closeToolStripMenuItem.Text = "Действия";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.закрытьToolStripMenuItem});
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.ОткрытьToolStripMenuItem_Click);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.ЗакрытьToolStripMenuItem_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "longitude";
+            // 
+            // lbl_longitude
+            // 
+            this.lbl_longitude.AutoSize = true;
+            this.lbl_longitude.Location = new System.Drawing.Point(94, 197);
+            this.lbl_longitude.Name = "lbl_longitude";
+            this.lbl_longitude.Size = new System.Drawing.Size(12, 17);
+            this.lbl_longitude.TabIndex = 3;
+            this.lbl_longitude.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 17);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "latitude";
+            // 
+            // lbl_latitude
+            // 
+            this.lbl_latitude.AutoSize = true;
+            this.lbl_latitude.Location = new System.Drawing.Point(94, 225);
+            this.lbl_latitude.Name = "lbl_latitude";
+            this.lbl_latitude.Size = new System.Drawing.Size(12, 17);
+            this.lbl_latitude.TabIndex = 3;
+            this.lbl_latitude.Text = "-";
+            // 
+            // lbl_timezona
+            // 
+            this.lbl_timezona.AutoSize = true;
+            this.lbl_timezona.Location = new System.Drawing.Point(94, 253);
+            this.lbl_timezona.Name = "lbl_timezona";
+            this.lbl_timezona.Size = new System.Drawing.Size(12, 17);
+            this.lbl_timezona.TabIndex = 3;
+            this.lbl_timezona.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 253);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 17);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "time zona";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(755, 356);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Google Maps";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txt_ipInfo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_city;
+        private System.Windows.Forms.Label lbl_region;
+        private System.Windows.Forms.Label lbl_country;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_latitude;
+        private System.Windows.Forms.Label lbl_longitude;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl_timezona;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Google_Maps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += (s, e) => { if (e.KeyValue == (char)Keys.Enter) Btn_Search_Click(btn_Search, null); };
+        }
+
+        void Btn_Search_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            string country;
+            string region;
+            string city;
+            string latitude;
+            string longitude;
+            string timezona;
+            try
+            {
+                using (WebClient wc = new WebClient())
+                    line = wc.DownloadString($"http://free.ipwhois.io/xml/{txt_ipInfo.Text}");
+                Match match = Regex.Match(line, "<country>(.*?)</country>(.*?)<region>(.*?)</region>(.*?)<city>(.*?)</city>(.*?)<latitude>(.*?)</latitude>(.*?)<longitude>-(.*?)</longitude>(.*?)<timezone>(.*?)</timezone>");
+                country = match.Groups[1].Value;
+                lbl_country.Text = country;
+                region = match.Groups[3].Value;
+                lbl_region.Text = region;
+                city = match.Groups[5].Value;
+                lbl_city.Text = city;
+                latitude = match.Groups[7].Value;
+                lbl_latitude.Text = latitude;
+                longitude = match.Groups[9].Value;
+                lbl_longitude.Text = longitude;
+                timezona = match.Groups[11].Value;
+                lbl_timezona.Text = timezona;
+                               
+                StringBuilder queryAddress = new StringBuilder();
+                queryAddress.Append("https://www.google.ru/maps?q=");
+                if (country != string.Empty)
+                    queryAddress.Append(country + "," + "+");
+                if (region != string.Empty)
+                    queryAddress.Append(region + "," + "+");
+                if (city != string.Empty)
+                    queryAddress.Append(city + "," + "+");
+                if (latitude != string.Empty)
+                    queryAddress.Append(latitude + "," + "+");
+                if (longitude != string.Empty)
+                    queryAddress.Append(longitude + "," + "+");
+                if (timezona != string.Empty)
+                    queryAddress.Append(timezona + "," + "+");
+
+                webBrowser1.Navigate(queryAddress.ToString());
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        void Txt_ipInfo_TextChanged(object sender, EventArgs e)
+        {
+            if(Regex.IsMatch(txt_ipInfo.Text, "[^0-9-.]"))
+            {
+                txt_ipInfo.Text = txt_ipInfo.Text.Remove(txt_ipInfo.Text.Length - 1);
+                txt_ipInfo.SelectionStart = txt_ipInfo.TextLength;
+            }
+        }
+
+        void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "Вы открыли приложение Google Maps";
+            notifyIcon1.BalloonTipText = "Уведомление: вы свернули Google Maps";
+            notifyIcon1.Text = "Google Maps";
+        }
+
+        void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void ОткрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        void ЗакрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
